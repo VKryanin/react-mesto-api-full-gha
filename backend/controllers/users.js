5ec3d7bc2ec8cbc7b7e9d6350239d485ce38f95d
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-07-08 21:14:31 +0300

    rework

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -13,24 +13,23 @@ const { UnauthorizedError } = require('../utils/errors/UnauthorizedError');
 const { NotFoundError } = require('../utils/errors/NotFoundError');
 const { EmailIsBusyError } = require('../utils/errors/EmailIsBusyError');
 
-const getUsers = async (req, res, next) => {
+const getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch((err) => next(err));
 };
 
-const getUserById = async (req, res, next) => {
+const getUserById = (req, res, next) => {
   User.findById(req.params.userId)
     .orFail(new NotFoundError('User with this ID was not found.'))
     .then((user) => res.send({ data: user }))
     .catch((err) => next(err));
 };
 
-const createUser = async (req, res, next) => {
+const createUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
-
   return bcrypt.hash(password, 10)
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
@@ -59,69 +58,40 @@ const login = (req, res, next) => {
     .catch((err) => next(err));
 };
 
-const getInfo = async (req, res, next) => {
-  try {
-    const user = await User
-      .findById(req.user._id);
-    if (user) {
-      res
-        .status(STATUS_OK)
-        .send({ data: user });
-    } else {
-      throw new NotFoundError('User is not found');
-    }
-  } catch (err) {
-    next(err);
-  }
+const getInfo = (req, res, next) => {
+  User.findById(req.user._id)
+    .orFail(new NotFoundError('User with this ID was not found.'))
+    .then((userData) => res.send({ data: userData }))
+    .catch((err) => next(err));
 };
 
-const updateProfile = async (req, res, next) => {
+const updateProfile = (req, res, next) => {
   const { name, about } = req.body;
-  try {
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      { name, about },
-      { new: true, runValidators: true },
-    );
-    if (user) {
-      res
-        .status(STATUS_OK)
-        .send(user);
-    } else {
-      throw new NotFoundError('User not found');
-    }
-  } catch (err) {
-    if (err instanceof IncorrectRequestError) {
-      next(new IncorrectRequestError('Data is incorrect'));
-    } else {
-      next(err);
-    }
-  }
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .orFail(new NotFound('User with this ID was not found.'))
+    .then((updateProfile) => res.send({ data: updateProfile }))
+    .catch((err) => {
+      if (err instanceof ValidationError) {
+        next(new IncorrectRequestError(err.message));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const updateAvatar = async (req, res, next) => {
   const { avatar } = req.body;
-  try {
-    const user = await User
-      .findByIdAndUpdate(
-        req.user._id,
-        { avatar },
-        { new: true, runValidators: true },
-      );
-    if (user) {
-      res
-        .status(STATUS_OK)
-        .send(user);
-    } else {
-      throw new NotFoundError('User not found');
-    }
-  } catch (err) {
-    if (err instanceof IncorrectRequestError) {
-      next(new IncorrectRequestError('Data is incorrect'));
-    } else {
-      next(err);
-    }
-  }
+
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .orFail(new NotFound('Incorrected user ID'))
+    .then((newAvatar) => res.send({ data: newAvatar }))
+    .catch((err) => {
+      if (err instanceof ValidationError) {
+        next(new IncorrectRequestError(err.message));
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports = {
