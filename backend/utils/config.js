677a15c03eb8bd41fa8e234ab3a053792c1d2fc0
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-07-09 00:09:24 +0300

    fix

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,4 +1,5 @@
-const security = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev';
+const isProduction = process.env.NODE_ENV === 'production';
+const security = isProduction ? process.env.JWT_SECRET : 'dev';
 const { MONGO } = process.env;
 const PORT = process.env.PORT || 3000;
 
