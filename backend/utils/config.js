6b6e3e7c147aae8523615735d22d34e812546f2e
Vitaly Kryanin <krianinvv@internet.ru>
2023-08-21 15:24:44 +0300

    edit port

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 const security = isProduction ? process.env.JWT_SECRET : 'dev';
 const { MONGO } = process.env;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 module.exports = {
   security, MONGO, PORT,
